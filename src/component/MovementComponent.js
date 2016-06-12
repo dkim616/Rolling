666bc041696fe38b91be59ddf13406244dba9827
Daniel Kim <daniel.kim.gw@gmail.com>
2016-06-12 00:52:25 -0400

    Working on movement component

--- src/component/MovementComponent.js
+++ src/component/MovementComponent.js
@@ -3,14 +3,10 @@ import src.Component.Component as Component;
 var MovementComponent = Class(Component, function(supr) {
 	this.init = function(context) {
 		supr(this, "init", arguments);
-
-		context.on("InputStart", bind(context, function() {
-			this.view.style.x += 10;
-		}));
 	};
 
 	this.update = function(context, dt) {
-
+		context.style.x += (0.3 * dt);
 	};
 });
 
